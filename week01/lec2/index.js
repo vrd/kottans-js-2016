b3cc3d0babbae41b701d2571135aef8a347cc9bb
vrd <vorachnog@gmail.com>
2016-05-25 00:13:06 +0300

    #3 save

--- week01/lec2/index.js
+++ week01/lec2/index.js
@@ -5,18 +5,26 @@ const PostHTML = require("posthtml")
 const html = `<p class="js-smth col-lg-12 margin5left">Text</p>`
 const newHtml =`<p data-js=smth class=margin5left>Text</p>`
 const bootstrapRegExp = /col\-lg\-12/i
-
+const datajsRegExp = /js\-\w+/i
 
 const plugin = tree => {
   tree.map( node => {
     let classes = node.attrs.class.split(" ")
     let newClasses = classes.reduce( (prev, cur) => {
       if (!bootstrapRegExp.test(cur)) {
-        prev[prev.length] = cur        
+        prev.class[prev.class.length] = cur        
+      }
+      else if (datajsRegExp.test(cur)) {
+        prev.datajs[prev.datajs.length] = cur
       }      
       return prev
-    }, [])
-    node.attrs.class = newClasses.join(" ")
+    }, {datajs: [], class: []})
+    if (newClasses.datajs) {
+      node.attrs.datajs = newClasses.datajs.join(" ")
+    }
+    if (newClasses.class) {
+      node.attrs.class = newClasses.class.join(" ")
+    }    
     return node       
   })
 } 
