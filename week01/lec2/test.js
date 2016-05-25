a82ae662981c1b92fc920b66177a6c8851adaa30
vrd <vorachnog@gmail.com>
2016-05-25 14:29:32 +0300

    #3 Implemented basic tests

--- week01/lec2/test.js
+++ week01/lec2/test.js
@@ -1,2 +1,41 @@
 "use strict"
 
+const PostHTML = require("posthtml")
+const plugin = require("./plugin")
+
+const tests = [
+{
+  in: '<p>Text</p>',
+  out: '<p>Text</p>' 
+},
+{
+  in: '<p class="someclass">Text</p>',
+  out: '<p class="someclass">Text</p>' 
+},
+{
+  in: '<p class="col-md-10">Text</p>',
+  out: '<p>Text</p>' 
+},
+{
+  in: '<p class="col-xs-push-1">Text</p>',
+  out: '<p>Text</p>' 
+},
+{
+  in: '<p class="js-some">Text</p>',
+  out: '<p data-js="some">Text</p>' 
+}
+]
+
+tests.forEach( (test, i) => {
+  PostHTML()
+    .use(plugin)
+    .process(test.in)
+    .then( result => {
+      if (result.html === test.out) {
+        console.log('Test ' + (i) +'\t\t[OK]')
+      } else {
+        console.log('! Test ' + (i) +'\t\t[ERROR]')
+      }
+    })
+}) 
+
