ab51acb034ff4ae0eff7579691ed384064342fa2
vrd <vorachnog@gmail.com>
2016-05-25 12:45:30 +0300

    #3 Basic functionality of plugin

--- week01/lec2/index.js
+++ week01/lec2/index.js
@@ -2,39 +2,40 @@
 
 const PostHTML = require("posthtml")
 
-const html = `<p class="js-smth col-lg-12 margin5left">Text</p>`
-const newHtml =`<p data-js=smth class=margin5left>Text</p>`
-const bootstrapRegExp = /col\-lg\-12/i
-const datajsRegExp = /js\-\w+/i
+const html = `<p class="js-smth kottansjs-smth col-lg-12 pricol-lg-12 col-xs-push-1 pricol-xs-push-144 margin5left">Text</p>`
+const bootstrapRegExp = /^col\-(xs|sm|md|lg)(\-(push|pull|offset))?\-\d\d?$/i
+const datajsRegExp = /^js\-\w+/i
 
 const plugin = tree => {
   tree.map( node => {
-    let classes = node.attrs.class.split(" ")
-    let newClasses = classes.reduce( (prev, cur) => {
-      if (!bootstrapRegExp.test(cur)) {
+    
+    let newClasses = node.attrs.class.split(" ").reduce( (prev, cur) => {
+      if (datajsRegExp.test(cur)) {
+        prev.datajs[prev.datajs.length] = cur.slice(3)
+      } else if (!bootstrapRegExp.test(cur)) {
         prev.class[prev.class.length] = cur        
-      }
-      else if (datajsRegExp.test(cur)) {
-        prev.datajs[prev.datajs.length] = cur
       }      
       return prev
     }, {datajs: [], class: []})
-    if (newClasses.datajs) {
-      node.attrs.datajs = newClasses.datajs.join(" ")
+
+    if (newClasses.datajs.length > 0) {
+      node.attrs['data-js'] = newClasses.datajs.join(" ")
     }
-    if (newClasses.class) {
+
+    if (newClasses.class.length > 0) {
       node.attrs.class = newClasses.class.join(" ")
-    }    
+    } else {
+      delete node.attrs.class
+    }   
+
     return node       
   })
 } 
 
-PostHTML([plugin])
-  //.use(plugin)
+PostHTML()
+  .use(plugin)
   .process(html)
   .then( result => {
-      console.log('\ntree: ' + JSON.stringify(result.tree))
-      console.log('\nhtml: ' + result.html)
-      //return tree
-    }
-  )
+      console.log('\ninput HTML:\n\n' + html)
+      console.log('\noutput HTML:\n\n' + result.html + '\n')
+    })
